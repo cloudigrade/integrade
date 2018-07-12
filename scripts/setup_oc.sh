4183c9206de536a8f6fe2e7ffde78f03f1da6a60
Elijah DeLee <kdelee@redhat.com>
2018-07-12 13:44:11 +0000

    Update setup_oc.sh

--- scripts/setup_oc.sh
+++ scripts/setup_oc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euvo pipefail
 
-curl -L -s -o /tmp/oc.tar.gz https://github.com/openshift/origin/releases/download/v3.7.2/openshift-origin-client-tools-v3.7.2-282e43f-linux-64bit.tar.gz
+curl -L -s -o /tmp/oc.tar.gz https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz
 
 tar zxvf /tmp/oc.tar.gz -C /tmp
 mkdir "${HOME}/oc"
