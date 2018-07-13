41501c05e8db846ae4b40d3bd874366d6be5e98f
Calvin Spealman <cspealma@redhat.com>
2018-07-13 12:31:29 -0400

    Broke tests into UI fixture steps

--- integrade/tests/ui/test_add_account.py
+++ integrade/tests/ui/test_add_account.py
@@ -18,15 +18,52 @@ from integrade.tests.api.v1 import urls
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skip()
-def test_clearing_name(selenium, ui_dashboard, ui_user):
+@pytest.fixture
+def ui_addacct_page1(selenium, ui_dashboard):
     browser, login = ui_dashboard
 
     btn_add_account = find_element_by_text(selenium, 'Add Account')
     btn_add_account.click()
 
     dialog = selenium.find_element_by_css_selector('[role=dialog]')
-    dialog_next = find_element_by_text(dialog, 'Next')
+
+    return {
+        'dialog': dialog,
+        'dialog_next': find_element_by_text(dialog, 'Next'),
+    }
+
+
+@pytest.fixture
+def ui_addacct_page2(selenium, ui_addacct_page1):
+    profile_name = 'My Account'
+
+    find_element_by_text(ui_addacct_page1['dialog'], 'Account Name').click()
+    input = selenium.execute_script('return document.activeElement')
+    input.send_keys(profile_name)
+
+    ui_addacct_page1['dialog_next'].click()
+
+    return ui_addacct_page1
+
+
+@pytest.fixture
+def ui_addacct_page3(selenium, ui_addacct_page2):
+    dialog = ui_addacct_page2['dialog']
+    dialog_next = ui_addacct_page2['dialog_next']
+
+    dialog_next.click()
+
+    dialog_add = find_element_by_text(dialog, 'Add')
+    assert dialog_add.get_attribute('disabled')
+
+    ui_addacct_page2['dialog_add'] = dialog_add
+    return ui_addacct_page2
+
+
+@pytest.mark.skip()
+def test_account_name_required(selenium, ui_addacct_page1, ui_user):
+    dialog = ui_addacct_page1['dialog']
+    dialog_next = ui_addacct_page1['dialog_next']
 
     assert dialog_next.get_attribute('disabled')
     find_element_by_text(dialog, 'Account Name').click()
@@ -38,31 +75,14 @@ def test_clearing_name(selenium, ui_dashboard, ui_user):
     assert dialog_next.get_attribute('disabled')
 
 
-def test_add_account(drop_account_data, selenium, ui_dashboard, ui_user):
-    browser, login = ui_dashboard
+def test_add_account(drop_account_data, selenium, ui_addacct_page3, ui_user):
+    dialog = ui_addacct_page3['dialog']
+    dialog_add = ui_addacct_page3['dialog_add']
+    wait = WebDriverWait(selenium, 10)
 
-    profile_name = 'My Account'
-    acct_arn = 'arn:aws:iam::543234867065:role/Cloud-Meter-role'
-
-    btn_add_account = find_element_by_text(selenium, 'Add Account')
-    btn_add_account.click()
-
-    dialog = selenium.find_element_by_css_selector('[role=dialog]')
-    dialog_next = find_element_by_text(dialog, 'Next')
-
-    assert dialog_next.get_attribute('disabled')
-    find_element_by_text(dialog, 'Account Name').click()
-    input = selenium.execute_script('return document.activeElement')
-    input.send_keys(profile_name)
-
-    assert not dialog_next.get_attribute('disabled')
-    dialog_next.click()
-
-    dialog_next.click()
-
-    dialog_add = find_element_by_text(dialog, 'Add')
     assert dialog_add.get_attribute('disabled')
 
+    acct_arn = 'arn:aws:iam::543234867065:role/Cloud-Meter-role'
     find_element_by_text(dialog, 'ARN').click()
     input = selenium.execute_script('return document.activeElement')
     input.send_keys(acct_arn)
@@ -79,32 +99,12 @@ def test_add_account(drop_account_data, selenium, ui_dashboard, ui_user):
     assert r['results'][0]['account_arn'] == 'arn:aws:iam::543234867065:role/Cloud-Meter-role'
 
 
-def test_invalid_arn(drop_account_data, selenium, ui_dashboard, ui_user):
-    browser, login = ui_dashboard
+def test_invalid_arn(drop_account_data, selenium, ui_addacct_page3, ui_user):
+    dialog = ui_addacct_page3['dialog']
+    dialog_add = ui_addacct_page3['dialog_add']
     wait = WebDriverWait(selenium, 10)
 
-    profile_name = 'My Account'
     acct_arn = 'arn:aws:iam::543234867065:role/Cloud-Meter-role-WRONG'
-
-    btn_add_account = find_element_by_text(selenium, 'Add Account')
-    btn_add_account.click()
-
-    dialog = selenium.find_element_by_css_selector('[role=dialog]')
-    dialog_next = find_element_by_text(dialog, 'Next')
-
-    assert dialog_next.get_attribute('disabled')
-    find_element_by_text(dialog, 'Account Name').click()
-    input = selenium.execute_script('return document.activeElement')
-    input.send_keys(profile_name)
-
-    assert not dialog_next.get_attribute('disabled')
-    dialog_next.click()
-
-    dialog_next.click()
-
-    dialog_add = find_element_by_text(dialog, 'Add')
-    assert dialog_add.get_attribute('disabled')
-
     find_element_by_text(dialog, 'ARN').click()
     input = selenium.execute_script('return document.activeElement')
     input.send_keys(acct_arn)
