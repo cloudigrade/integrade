7879eda3f25d098eb0273f4b8c8843ada20e442b
Calvin Spealman <cspealma@redhat.com>
2018-07-16 09:42:43 -0400

    Remove unused utility

--- integrade/utils.py
+++ integrade/utils.py
@@ -24,8 +24,3 @@ def gen_password(length=20):
 def uuid4():
     """Provide unique string identifiers."""
     return str(uuid.uuid4())
-
-
-def get_primary_account_id():
-    """Return the account ID for the primary AWS account."""
-    return boto3.client('sts').get_caller_identity().get('Account')
