0a933640d1cface08533376d7dd345be2ff14053
Elijah DeLee <kdelee@redhat.com>
2018-07-26 13:52:37 -0400

    Leading - breaks oc-auth.sh for test env

--- scripts/oc-auth.sh
+++ scripts/oc-auth.sh
@@ -3,7 +3,11 @@ alias uuid4='python -c "import uuid;print(uuid.uuid4())"'
 export CLOUDIGRADE_BASE_URL="${CLOUDIGRADE_BASE_URL:-test.cloudigra.de}"
 export CLOUDIGRADE_USER=$(uuid4)
 export CLOUDIGRADE_PASSWORD=$(uuid4)
-export CLOUDIGRADE_POD_NAME=${DEPLOYMENT_PREFIX}-cloudigrade-api
+if [ $DEPLOYMENT_PREFIX = "" ]; then
+	export CLOUDIGRADE_POD_NAME=cloudigrade-api
+else
+	export CLOUDIGRADE_POD_NAME=${DEPLOYMENT_PREFIX}-cloudigrade-api
+fi
 oc rsh -c ${CLOUDIGRADE_POD_NAME} $(oc get pods -o jsonpath='{.items[*].metadata.name}' -l name=${CLOUDIGRADE_POD_NAME}) scl enable rh-postgresql96 rh-python36 -- python manage.py createsuperuser --no-input --username $CLOUDIGRADE_USER --email="${CLOUDIGRADE_USER}@example.com"
 export CLOUDIGRADE_TOKEN=$(oc rsh -c ${CLOUDIGRADE_POD_NAME} $(oc get pods -o jsonpath='{.items[*].metadata.name}' -l name=${CLOUDIGRADE_POD_NAME}) scl enable rh-postgresql96 rh-python36 -- python manage.py drf_create_token $CLOUDIGRADE_USER | awk '{print $3}')
 cat << EOF | oc rsh -c ${CLOUDIGRADE_POD_NAME} $(oc get pods -o jsonpath='{.items[*].metadata.name}' -l name=${CLOUDIGRADE_POD_NAME}) scl enable rh-postgresql96 rh-python36 -- python manage.py shell
