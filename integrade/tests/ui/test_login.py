ba07ad85814331e14484f0c53a13c5c343769843
Calvin Spealman <cspealma@redhat.com>
2018-08-17 14:15:43 -0400

    oops

--- integrade/tests/ui/test_login.py
+++ integrade/tests/ui/test_login.py
@@ -56,7 +56,6 @@ def test_login_valid_username(selenium, ui_loginpage, ui_user):
 
     # Username field becomes valid with an e-mail address entered as username
     assert browser.execute_script(CHECK_VALID)
-    assert 0, ui_user
 
 
 def test_incorrect_login(selenium, ui_loginpage, ui_user):
