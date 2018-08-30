c84cc8531edcfa14a4c54fc3365a01f7c20393c5
Elijah DeLee <kdelee@redhat.com>
2018-08-30 15:07:28 -0400

    Remove tests for removed api endpoint

--- integrade/tests/api/v1/test_user_accounts.py
+++ integrade/tests/api/v1/test_user_accounts.py
@@ -56,59 +56,6 @@ def test_create_with_email():
     })
 
 
-def test_change_password():
-    """Ensure an user can change its account password.
-
-    :id: 43a5ebd9-9f19-446d-8b7f-fccb7870c90d
-    :description: Ensure an user can change its account password, it can login
-        using the new password and can make requests using the authentication
-        token obtained with the new password.
-    :steps:
-        1) With an authenticated user, send a post request to /auth/password/
-           with the current password and the new password.
-        2) Ensure that the user can generate an authentication token with the
-           new password and can make requests.
-    :expectedresults:
-        1) Assert the server returns a 204 response..
-        2) Assert the user can login with the new password and can access
-           /auth/me/
-    """
-    user = create_user_account()
-    new_password = gen_password()
-    client = api.Client()
-    payload = {
-        'current_password': user['password'],
-        'new_password': new_password,
-    }
-
-    response = client.post(urls.AUTH_TOKEN_CREATE, user)
-    assert response.status_code == 200
-    json_response = response.json()
-    assert 'auth_token' in json_response
-    auth = api.TokenAuth(json_response['auth_token'])
-
-    response = client.post(urls.AUTH_PASSWORD, payload, auth=auth)
-    assert response.status_code == 204
-
-    response = client.post(urls.AUTH_TOKEN_DESTROY, {}, auth=auth)
-    assert response.status_code == 204
-
-    user['password'] = new_password
-    response = client.post(urls.AUTH_TOKEN_CREATE, user, auth=None)
-    assert response.status_code == 200
-    json_response = response.json()
-    assert 'auth_token' in json_response
-    auth.token = json_response['auth_token']
-
-    response = client.get(urls.AUTH_ME, auth=auth)
-    assert response.status_code == 200
-    json_response = response.json()
-    assert json_response['username'] == user['username']
-
-    response = client.post(urls.AUTH_TOKEN_DESTROY, {}, auth=auth)
-    assert response.status_code == 204
-
-
 def test_user_list(drop_account_data):
     """Super users can request lists of created user accounts.
 
