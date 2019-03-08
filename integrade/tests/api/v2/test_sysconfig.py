0b75c758d5d392799ba4cfd1268ac6b725d91e6b
Maribeth Pierce <mpierce@redhat.com>
2019-03-08 17:42:01 -0500

    initial test of sysconfig for 3scale auth

--- integrade/tests/api/v2/test_sysconfig.py
+++ integrade/tests/api/v2/test_sysconfig.py
@@ -12,20 +12,18 @@
 import logging
 
 import pytest
-
+import re
 import requests
 
-
-logger = logging.getLogger(__name__)
-
+from integrade import api
+from integrade.tests import urls
+from integrade.tests.utils import get_auth
 
 def is_on_local_network():
-    """Check if on internal RH network.
+    """Check if on internal RH network. This matters because we can ONLY access
+    3scale from inside Red Hat network. API V2 tests should be skipped if this
+    returns False - ie. if running in gitlab CI"""
 
-    This matters because we can ONLY access 3scale from inside RedHat network
-    API V2 tests should be skipped if this returns False - ie. if running in
-    gitlab CI.
-    """
     url = 'https://api.access.stage.cloud.paas.upshift.redhat.com'
     try:
         requests.get(url, verify=False)
@@ -33,8 +31,6 @@ def is_on_local_network():
         logging.warning(e)
         return False
     return True
-
-
 
 @pytest.mark.skipif(not is_on_local_network(),
                     reason='Can\'t run outside of local RH network')
@@ -48,32 +44,6 @@ def test_sysconfig():
     :expectedresults: The server returns a 200 response with the expected
         configuration information.
     """
-
-    qa = 'api.access.qa.cloud.paas.upshift.redhat.com'
-    qa_end = '/r/insights/platform/cloudigrade/api/v2/sysconfig/'
-    creds = ('mpierce@redhat.com', 'redhat')
-    V2_QA_URL = f'https://{qa}{qa_end}'
-    qa_branch = '3scale-investigation'
-    qa_url = V2_QA_URL
-    test_headers = {'X-4Scale-Env': 'ci', 'X-4Scale-Branch': qa_branch}
-    qa_response = requests.get(
-                    qa_url,
-                    auth=creds,
-                    headers=test_headers,
-                    verify=False
-                )
-
-    stage = 'api.access.stage.cloud.paas.upshift.redhat.com'
-    stage_end = '/r/insights/platform/cloudigrade/auth/"'
-    V2_STAGE_URL = f'https://{stage}{stage_end}'
-    stage_url = V2_STAGE_URL
-    stage_headers = {'X-4Scale-Env': 'qa'}
-    stage_response = requests.get(
-                        stage_url,
-                        auth=creds,
-                        headers=stage_headers,
-                        verify=False
-                    )
 
     qa = 'api.access.qa.cloud.paas.upshift.redhat.com'
     qa_end = '/r/insights/platform/cloudigrade/api/v2/sysconfig/'
