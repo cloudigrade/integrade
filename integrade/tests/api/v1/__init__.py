5ef8225af3c2341cd1a0dbc8228c002b5a1a1e1a
Maribeth Pierce <mpierce@redhat.com>
2019-03-11 10:34:04 -0400

    Add __init__.py files so we have packages

---
+++ integrade/tests/api/v1/__init__.py
@@ -0,0 +1,6 @@
+"""Tests for cloudigrade v1.
+
+This package and its sub-packages contain tests for cloudigrade API v1.
+These tests should be run against a cloudigrade installation. These tests are
+entirely different from the tests in v2.
+"""
---
+++ integrade/tests/api/v2/__init__.py
@@ -0,0 +1,6 @@
+"""Tests for cloudigrade v2.
+
+This package and its sub-packages contain tests for cloudigrade API v2.
+These tests should be run against a cloudigrade installation. These tests are
+entirely different from the tests in v1.
+"""
