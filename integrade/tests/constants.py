a7bace71babcdd94d530f70e31fb06243ba06585
Maribeth Pierce <mpierce@redhat.com>
2019-06-17 13:13:14 -0400

    update URL for api

--- integrade/tests/constants.py
+++ integrade/tests/constants.py
@@ -7,8 +7,8 @@ AWS_ACCOUNT_TYPE = 'AwsAccount'
 EC2_TERMINATED_CODE = 48
 """Terminated EC2 instances have the state code of 48."""
 
-QA_URL = 'https://qa.cloud.paas.upshift.redhat.com' \
+QA_URL = 'https://qa.cloud.paas.psi.redhat.com' \
     '/api/cloudigrade/v2/'
 
-STAGE_URL = 'https://stage.cloud.paas.upshift.redhat.com' \
+STAGE_URL = 'https://stage.cloud.paas.psi.redhat.com' \
     '/api/cloudigrade/v2/'
